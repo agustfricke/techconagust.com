b83c88b02115fe93c7c0f8774f9ac1485f18121e
Agust <agustfricke@protonmail.com>
2023-09-17 19:44:07 -0300

    banner landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,46 @@
-import { defineConfig } from 'astro/config';
-
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
+import remarkCodeTitles from "remark-code-titles";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank",
+      rel: ["noreferrer noopener"],
+      content: {
+        type: "text",
+        value: "↗"
+      }
+    }], rehypeSlug, [rehypeAutolinkHeadings, {
+      properties: {
+        class: "heading-link heading-link--hidden---effects",
+        "data-heading-link": true
+      },
+      behavior: "wrap"
+    }]],
+    shikiConfig: {
+      theme: "rose-pine",
+      wrap: true
+    }
+  },
+  integrations: [react(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), mdx(), tailwind()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
