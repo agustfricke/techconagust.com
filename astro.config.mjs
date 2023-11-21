65b385ff70639e6456c1c152662d9d27f31e3c0f
agustfricke <agustfricke@protonmail.com>
2023-11-21 20:34:07 -0300

    batman astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,8 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeExternalLinks from "rehype-external-links";
-import rehypeSlug from "rehype-slug";
-import remarkCodeTitles from "remark-code-titles";
+import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  markdown: {
-    remarkPlugins: [remarkCodeTitles],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: "_blank",
-      rel: ["noreferrer noopener"],
-      content: {
-        type: "text",
-        value: "↗"
-      }
-    }], rehypeSlug, [rehypeAutolinkHeadings, {
-      properties: {
-        class: "heading-link heading-link--hidden---effects",
-        "data-heading-link": true
-      },
-      behavior: "wrap"
-    }]],
-    shikiConfig: {
-      theme: "rose-pine",
-      wrap: true
-    }
-  },
-  integrations: [react(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), mdx(), tailwind()],
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
+  integrations: [tailwind()]
 });
