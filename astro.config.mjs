a491f85661a7168e276790988c164bab25d8c9fc
agust <agust@MacBook-Air-de-Carlos.local>
2023-11-26 17:34:19 -0300

    add starlight theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,106 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
+export const locales = {
+	root: { label: 'English', lang: 'en' },
+	es: { label: 'Español', lang: 'es' },
+};
+
+/* https://vercel.com/docs/projects/environment-variables/system-environment-variables#system-environment-variables */
+const VERCEL_PREVIEW_SITE =
+	process.env.VERCEL_ENV !== 'production' &&
+	process.env.VERCEL_URL &&
+	`https://${process.env.VERCEL_URL}`;
+
+const site = VERCEL_PREVIEW_SITE || 'https://starlight.astro.build/';
+
 export default defineConfig({
+	site,
+	trailingSlash: 'always',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Starlight',
+			logo: {
+				light: '/src/assets/logo-light.svg',
+				dark: '/src/assets/logo-dark.svg',
+				replacesTitle: true,
+			},
+			editLink: {
+				baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/',
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
+				discord: 'https://astro.build/chat',
 			},
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						src: 'https://cdn.usefathom.com/script.js',
+						'data-site': 'EZBHTSIG',
+						defer: true,
+					},
+				},
+				{
+					tag: 'meta',
+					attrs: { property: 'og:image', content: site + 'og.jpg?v=1' },
+				},
+				{
+					tag: 'meta',
+					attrs: { property: 'twitter:image', content: site + 'og.jpg?v=1' },
+				},
+			],
+			customCss: process.env.NO_GRADIENTS ? [] : ['./src/assets/landing.css'],
+			locales,
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Start Here',
+					translations: {
+						es: 'Comienza aqui',
+					},
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{
+							label: 'Getting Started',
+							link: 'getting-started',
+							translations: {
+								es: 'Empezando',
+							},
+						},
+						{
+							label: 'Manual Setup',
+							link: 'manual-setup',
+							translations: {
+								es: 'Configuración Manual',
+							},
+						},
+						{
+							label: 'Environmental Impact',
+							link: 'environmental-impact',
+							translations: {
+								es: 'Documentación ecológica',
+							},
+						},
+						{
+							label: 'Showcase',
+							link: 'showcase',
+							translations: {
+								es: 'Caso Show',
+							},
+						},
 					],
 				},
 				{
+					label: 'Guides',
+					translations: {
+						es: 'Guías',
+					},
+					autogenerate: { directory: 'guides' },
+				},
+				{
 					label: 'Reference',
+					translations: {
+						es: 'Referencias',
+					},
 					autogenerate: { directory: 'reference' },
 				},
 			],
