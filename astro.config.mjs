2c7e33b589770e5bb134868cbe8b5af9b851c322
agust <agust@MacBook-Air-de-Carlos.local>
2023-11-27 19:03:22 -0300

    batman

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,69 +1,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-export const locales = {
-	root: { label: 'English', lang: 'en' },
-	es: { label: 'Español', lang: 'es' },
-};
-
-const VERCEL_PREVIEW_SITE =
-	process.env.VERCEL_ENV !== 'production' &&
-	process.env.VERCEL_URL &&
-	`https://${process.env.VERCEL_URL}`;
-
-const site = VERCEL_PREVIEW_SITE || 'https://zustex.com';
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Machine Lab',
+			title: 'AgusTech',
 			social: {
-				github: 'https://github.com/agustfricke',
-				youtube: 'https://youtube.com/@techconagust',
-        "x.com": 'https://x.com/agustfricke.com'
+				github: 'https://github.com/withastro/starlight',
+        youtube: 'https://youtube.com',
+        "x.com": "https://x.com/TechConAgust",
 			},
-			head: [
-				{
-					tag: 'script',
-					attrs: {
-						src: 'https://cdn.usefathom.com/script.js',
-						'data-site': 'EZBHTSIG',
-						defer: true,
-					},
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'og:image', content: site + 'og.jpg?v=1' },
-				},
-				{
-					tag: 'meta',
-					attrs: { property: 'twitter:image', content: site + 'og.jpg?v=1' },
-				},
-			],
-			customCss: process.env.NO_GRADIENTS ? [] : ['./src/assets/landing.css'],
-			locales,
 			sidebar: [
 				{
-					label: 'Go programing',
-					translations: {
-						es: 'Programacion con Go',
-					},
+					label: 'Programacion con Go',
 					autogenerate: { directory: 'go' },
-        },
-				{
-					label: 'Go Projects',
-					translations: {
-						es: 'Proyectos en Go',
-					},
-					autogenerate: { directory: 'go-projects' },
 				},
 				{
-					label: 'Tools',
-					translations: {
-						es: 'Herramientas',
-					},
-					autogenerate: { directory: 'tools' },
+					label: 'Proyectos con Go',
+					autogenerate: { directory: 'proyeyctos-go' },
+				},
+				{
+					label: 'Herramientas',
+					autogenerate: { directory: 'herramientas' },
 				},
 			],
 		}),
